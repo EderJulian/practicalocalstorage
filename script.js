f9e82ad246146d53f6f42e096e6dd604a4042ac2
ederjulian <julianeder97@gmail.com>
2023-10-21 00:20:43 +0200

    Done

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-// Escucha el envío del formulario
+
 document.getElementById("contactForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    // Obtiene los valores de los campos
-    var nombre = document.getElementById("nombre").value;
-    var correo = document.getElementById("correo").value;
-    var mensaje = document.getElementById("mensaje").value;
+  
+    const nombre = document.getElementById("nombre").value;
+    const correo = document.getElementById("correo").value;
+    const mensaje = document.getElementById("mensaje").value;
 
-    // Obtiene la lista de contactos existentes o crea una si no existe
-    var contactos = JSON.parse(localStorage.getItem("contactos")) || [];
+   
+    const contactos = JSON.parse(localStorage.getItem("contactos")) || [];
 
-    // Crea un objeto con los datos del usuario
-    var usuario = {
+ 
+    const usuario = {
         nombre: nombre,
         correo: correo,
         mensaje: mensaje
     };
 
-    // Agrega el nuevo usuario a la lista de contactos
+    
     contactos.push(usuario);
 
-    // Guarda la lista actualizada en el Local Storage
+ 
     localStorage.setItem("contactos", JSON.stringify(contactos));
 
-    // Muestra los contactos guardados en el DOM
+    
     mostrarContactos();
 
-    // Limpia el formulario
+   
     document.getElementById("contactForm").reset();
 });
 
-// Función para mostrar los contactos guardados en el DOM
+
 function mostrarContactos() {
-    var contactos = JSON.parse(localStorage.getItem("contactos"));
+    const contactos = JSON.parse(localStorage.getItem("contactos"));
 
     if (contactos) {
-        var userDataDiv = document.getElementById("userData");
+        const userDataDiv = document.getElementById("userData");
         userDataDiv.innerHTML = "Contactos guardados en Local Storage:<br>";
 
         contactos.forEach(function (contacto, index) {
@@ -49,7 +49,7 @@ function mostrarContactos() {
 
 mostrarContactos();
 
-// Botón para borrar todos los contactos
+
 document.getElementById("borrarContactos").addEventListener("click", function () {
     localStorage.removeItem("contactos");
     mostrarContactos();
